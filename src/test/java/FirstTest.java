f9d43a6d2bcf2ad304dc179da0aaac58f5409ebf
pavelsaz <dr.patrick@inbox.ru>
2024-03-03 15:27:28 +0100

    dependencies have been added to pom.xml

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,13 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void FirstTest() {
+        Assert.assertTrue(true);
+    }
+}
+
+
+
