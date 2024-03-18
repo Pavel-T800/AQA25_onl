416186f94cb242951942273db06c165b0a77d86b
pavelsaz <dr.patrick@inbox.ru>
2024-03-18 09:55:24 +0100

    Init

---
+++ src/test/java/CalcBaseTest.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeSuite;
+
+public class CalcBaseTest {
+    protected Calculator calculator;
+
+    @BeforeSuite
+    public void initCalculator() {
+        calculator = new Calculator();
+    }
+
+    @BeforeGroups(groups = {"general", "extended"})
+    public void methodBeforeGeneral() {
+        calculator = new Calculator();
+    }
+}
